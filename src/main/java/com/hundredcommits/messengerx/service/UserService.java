03941a63234e8e9630d9d9da91874dea3ef8847e
szogunn <mateusz1_boczek1@interia.pl>
2024-03-08 00:07:00 +0100

    Created Utils class for Security. Deleted getAuthenticatedUser() method from User Service. Checking with thymeleaf if user is authenticated

--- src/main/java/com/hundredcommits/messengerx/service/UserService.java
+++ src/main/java/com/hundredcommits/messengerx/service/UserService.java
@@ -1,6 +1,5 @@
 package com.hundredcommits.messengerx.service;
 
-import com.hundredcommits.messengerx.domains.User;
 import com.hundredcommits.messengerx.dtos.UserDTO;
 import com.hundredcommits.messengerx.payloads.JwtInfoResponse;
 import com.hundredcommits.messengerx.payloads.LoginRequest;
@@ -10,5 +9,4 @@ public interface UserService {
 
     UserDTO signUp(SignupRequest signupRequest);
     JwtInfoResponse logIn(LoginRequest loginRequest);
-    User getAuthenticatedUser();
 }
---
+++ src/main/java/com/hundredcommits/messengerx/utils/SecurityUtils.java
@@ -0,0 +1,17 @@
+package com.hundredcommits.messengerx.utils;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SecurityUtils {
+
+    private SecurityUtils() {
+    }
+
+    public static boolean isAuthenticated() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication != null && authentication.isAuthenticated() && !"anonymousUser".equals(authentication.getPrincipal());
+    }
+}
