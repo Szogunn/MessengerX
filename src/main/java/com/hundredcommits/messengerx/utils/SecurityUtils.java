b75e15714ba2e812ed11637cf5248b07c41bc412
szogunn <mateusz1_boczek1@interia.pl>
2024-03-13 22:16:01 +0100

    Changed logic in method which return authenticated User

--- src/main/java/com/hundredcommits/messengerx/utils/SecurityUtils.java
+++ src/main/java/com/hundredcommits/messengerx/utils/SecurityUtils.java
@@ -1,7 +1,9 @@
 package com.hundredcommits.messengerx.utils;
 
+import com.hundredcommits.messengerx.jwt.UserDetailsImpl;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -17,6 +19,16 @@ public class SecurityUtils {
 
     public static String getAuthenticatedUsername() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        return authentication.getName();
+
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return null;
+        }
+
+        Object principal = authentication.getPrincipal();
+        if (principal instanceof UserDetails) {
+            return ((UserDetailsImpl) principal).getUsername();
+        }
+
+        return principal.toString();
     }
 }
