73ce6a8ef07bdd5b9cde3bcf833854ab12e606a1
szogunn <mateusz1_boczek1@interia.pl>
2024-04-04 20:59:07 +0200

    Fixed errors with fetching Invitation objects from db. Implemented "wither" design pattern for final fields.

--- src/main/java/com/hundredcommits/messengerx/domains/Invitation.java
+++ src/main/java/com/hundredcommits/messengerx/domains/Invitation.java
@@ -16,12 +16,39 @@ public class Invitation {
     private boolean accepted;
     private Date responseDate;
 
+    private Invitation() {
+        this.invitationDate = null;
+        this.fromUser = null;
+        this.toUser = null;
+    }
+
     public Invitation(String fromUser, String toUser) {
         this.fromUser = fromUser;
         this.toUser = toUser;
         this.invitationDate = new Date(System.currentTimeMillis());
         this.accepted = false;
         this.responseDate = null;
+    }
+
+    private Invitation(String id, String fromUser, String toUser, Date invitationDate, boolean accepted, Date responseDate) {
+        this.id = id;
+        this.fromUser = fromUser;
+        this.toUser = toUser;
+        this.invitationDate = invitationDate;
+        this.accepted = accepted;
+        this.responseDate = responseDate;
+    }
+
+    public Invitation withInvitationDate(Date invitationDate) {
+        return new Invitation(this.id, this.fromUser, this.toUser, invitationDate, this.accepted, this.responseDate);
+    }
+
+    public Invitation withFromUser(String fromUser) {
+        return new Invitation(this.id, fromUser, this.toUser, this.invitationDate, this.accepted, this.responseDate);
+    }
+
+    public Invitation withToUser(String toUser) {
+        return new Invitation(this.id, this.fromUser, toUser, this.invitationDate, this.accepted, this.responseDate);
     }
 
     public String getFromUser() {
